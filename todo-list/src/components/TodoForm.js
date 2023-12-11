40de86f1b893ee37b32a75c5eea3af948e12911b
ramanmand <rmand313@gmail.com>
2023-12-11 13:45:16 -0500

    arrow function added to get user input

--- todo-list/src/components/TodoForm.js
+++ todo-list/src/components/TodoForm.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React , {useState} from "react";
 
 
 export const TodoForm = () => {
+  const [value, setValue] = useState("")
   return (
     <form className="TodoForm">
-      <input type="text" className="todo-input" placeholder="Enter yout task here"/>
+      <input type="text" className="todo-input" placeholder="Enter yout task here" onChange={(e) => console.log(e.target.value)}/>
       <button type="submit" className="todo-btn">Add Task</button>
       </form>
   )
