04a1ba6005de54be31df51d5fde86594fac45a9c
ramanmand <rmand313@gmail.com>
2023-12-11 13:51:30 -0500

    added a fn to save the value of state and to prevent reloading by default

--- todo-list/src/components/TodoForm.js
+++ todo-list/src/components/TodoForm.js
@@ -3,9 +3,13 @@ import React , {useState} from "react";
 
 export const TodoForm = () => {
   const [value, setValue] = useState("")
+  const handleSubmit = e => { //fn to capture the state upon form submission
+    e.preventDefault(); //prevents reloading of the page when task is submitted
+    console.log(value);
+  }
   return (
-    <form className="TodoForm">
-      <input type="text" className="todo-input" placeholder="Enter yout task here" onChange={(e) => console.log(e.target.value)}/>
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input type="text" className="todo-input" placeholder="Enter yout task here" onChange={(e) => setValue(e.target.value)}/>
       <button type="submit" className="todo-btn">Add Task</button>
       </form>
   )
