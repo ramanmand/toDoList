f5eb56c784ce8e0a9f7846e9910c1572b1c9fd56
ramanmand <rmand313@gmail.com>
2023-12-11 14:12:37 -0500

    fixed errors

--- todo-list/src/components/TodoBox.js
+++ todo-list/src/components/TodoBox.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React , {useState} from "react";
 import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
 
 export const TodoBox = () => {
+      const [todos, setTodos] = useState([]); //this will parse the value from todoForm
+      const addTodo = todo => {
+        setTodos([...todos, {id: uuidv4(), task:todo, completed:false, isEditing:false}])
+      }
   return (
     <div className= "TodoBox">
-      <TodoForm />
+      <TodoForm addTodo= {addTodo} /> {/*prop to parse the value*/}
     </div>
   )
 }
